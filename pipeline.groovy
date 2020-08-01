676b4bc55904901adfe14e278ffdedacc0367e4a
ApurbaPaul <apurvon@gmail.com>
2020-08-01 14:51:45 +0530

    add dsl job

---
+++ pipeline.groovy
@@ -0,0 +1,36 @@
+
+
+pipeline {
+    agent {
+        node {
+          label 'master'
+        }
+    }
+    environment {
+        CI = 'true'
+    }
+    stages {
+        stage('Build') {
+            steps {
+                sh 'npm install'
+            }
+        }
+        stage('Test') {
+            steps {
+                sh './scripts/test.sh'
+            }
+        }
+        stage ('test shared library') {
+            steps {
+                sample "Apurba"
+            }
+        }
+        stage('Deliver') {
+            steps {
+                sh './scripts/deliver.sh'
+                input message: 'Finished using the web site? (Click "Proceed" to continue)'
+                sh './scripts/kill.sh'
+            }
+        }
+    }
+}
